f89e86ca8a2b045a01d547247ae963d00636e90f
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-07-21 22:57:57 +0200

    .

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -1,11 +1,37 @@
 <script>
-	var x = 2
+	var table = Array(20).fill("").map(v => Array(20).fill(""))
+	/**
+     * @param {{ target: { id: { split: (arg0: string) => [undefined, Number, Number]; }; }; }} e
+     */
+	function f(e) {
+		var x, y
+		[,x,y] = e.target.id.split("o")
+		table[y][x] = "O"
+	}
 </script>
 
 <div>
-	{x}
+	<table>
+		{#each table as row, y}
+		<tr>
+			{#each row as cell, x}
+				<td on:click={f} id=o{x}o{y}o>{cell}</td>
+			{/each}
+		</tr>
+		{/each}
+	</table>
+
 </div>
 
 <style>
-
+	td {
+		width: 20px;
+		height: 20px;
+		border: solid 1px black;
+		user-select: none;
+		cursor: pointer;
+	}
+	table {
+		margin: auto;
+	}
 </style>
