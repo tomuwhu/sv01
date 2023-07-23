0fb93b1f4aafc6a4028d37cb0954377567d345ce
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-07-23 20:50:44 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,70 @@
 <script>
 	import Amoba from './Amoba.svelte'
   import Valasz from './Valasz.svelte'
-	var ap = 1, h1 = ["Amőba", "Válasz mindenre"]
+	var ap = 1, h1 = ["Amőba", "Válasz mindenre", "Mindkettő"]
+  $: hap = h1[ap]
 </script>
 
 <svelte:head>
-	<title>Svelte gyakorlatok</title>
+	<title>{hap}</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-  {#if ap > 0}
-    <button on:click={() => ap--}>{h1[ap - 1]}</button>
-  {/if}
-  <h1>{h1[ap]}</h1>
-  {#if ap < h1.length - 1}
-    <button on:click={() => ap++ }>{h1[ap + 1]}</button>
-  {/if}
-  <hr>
-  <div class:hide={ap != 0}><Amoba /></div>
-  <div class:hide={ap != 1}><Valasz /></div>
+  <div class="menu">
+    {#each h1 as elem, i}
+      {#if ap == i}
+        <button class="a">{hap}</button>
+      {:else}
+        <button 
+          on:click={() => ap = i }
+          class="p"
+        >{h1[i]}</button>
+      {/if}
+    {/each}
+  </div>
+  <div class:hide={ap == 0}><Valasz /></div>
+  <br>
+  <hr  class:hide={ap != 2}>
+  <div class:hide={ap == 1}>
+    {#if ap==0}
+      <h1>Amőba egyedül</h1>
+      <br>
+    {:else}
+      <br>
+    {/if}
+    <Amoba />
+  </div>
 </section>
 
 <style>
   * {
-        user-select: none;
-  }
-  h1, button {
-    font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
-    font-weight: 300;
-    font-size: 20px;
-  }
-  h1 {
-    display: inline-block;
+    user-select: none;
+    margin: 0px;
   }
   button {
-    cursor: pointer;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-weight: 300;
+    font-size: 15px;
+    padding: 6px;
+    background: transparent;
     color: gray;
     border: none;
   }
-  button:hover {
-    color: red;
+  button.a {
+    color: rgb(121, 29, 29);
+    background-color: rgb(125, 155, 125);
+  }
+  button.p:hover {
+    cursor: pointer;
+    color: rgb(166, 95, 95);
   }
   .hide {
     display: none;
+  }
+  .menu {
+    padding: 0px;
+    background-color: rgb(181, 223, 209);
+    border-bottom: solid 1px black;
   }
 </style>
