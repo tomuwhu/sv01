3b64b71736c0232e2a1f6091ce0e81094040a2d2
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-07-31 19:31:23 +0200

    .

---
+++ src/routes/Array/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  var t = Array(10)
+    .fill(0)
+    .map((v, i) => 2 ** i);
+  var i;
+</script>
+
+<table>
+  <tr>
+    <th class="h">i: </th>
+    <th class="h">2<sup>i</sup></th>
+  </tr>
+  {#each t as o, i}
+    <tr>
+      <td>{i + 1}: </td>
+      <td>{o}</td>
+    </tr>
+  {/each}
+</table>
+
+<style>
+  table {
+    border-spacing: 6px;
+  }
+  td,
+  th {
+    border: solid 1px black;
+    text-align: right;
+    padding: 6px;
+    border-radius: 6px;
+    width: 60px;
+  }
+</style>
---
+++ src/routes/Counter/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  var a = 0;
+  $: title = `A számláló jelenlegi értéke: ${a}`;
+</script>
+
+<button on:click={() => a++} {title}>{a}</button>
+
+<style>
+  button {
+    font-size: 20px;
+    cursor: pointer;
+  }
+</style>
